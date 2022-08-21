f728b4d1f128e1f21b81f1398f7a70914bebd801
Oksana Konieva <anaska0489@gmail.com>
2022-08-21 21:30:11 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -8,4 +8,4 @@ itemEl.forEach(function (element) {
     console.log("Category:", element.firstElementChild.textContent);
     
     console.log("Elements:", element.lastElementChild.children.length);
-})
+}) 
